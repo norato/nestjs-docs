4f74efda83bb9d818630b8b9618e681b68932205
Felipe Norato <fnoratol@gmail.com>
2018-10-05 21:00:07 -0300

    Fix test

--- src/cats/services/cats.service.spec.ts
+++ src/cats/services/cats.service.spec.ts
@@ -1,15 +1,17 @@
+import { DatabaseModule } from './../../database/database.module';
+import { catsProviders } from './../cat.providers';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CatsService } from './cats.service';
 import { Model } from 'mongoose';
 import { Cat } from '../interfaces';
-import { getModelToken } from '@nestjs/mongoose';
 
 describe('CatsService', () => {
   let service: CatsService;
   let catModel: Model<Cat>;
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CatsService],
+      imports: [DatabaseModule],
+      providers: [...catsProviders, CatsService],
     }).compile();
     service = module.get<CatsService>(CatsService);
   });
